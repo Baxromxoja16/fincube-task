c9b9830a0ce3143dea9d001d3c99090ca189ab31
baxromxoja16 <aglamof@mail.ru>
2024-04-07 11:20:47 +0500

    feat: added route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { PagenotfoundComponent } from './shared/components/pagenotfound/pagenotfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/auth',
+    pathMatch: 'full',
+  },
+  { path: 'auth', component: LoginComponent },
+  { path: '**', pathMatch: 'full',
+  component: PagenotfoundComponent },
+];
