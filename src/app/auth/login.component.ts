c44a4a7a4575f51fd8ca5c15f6e7c75029b80df1
baxromxoja16 <aglamof@mail.ru>
2024-04-07 12:58:16 +0500

    feat: added login validation changed

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent {
 
   constructor() {
     this.logInForm = new FormGroup({
-      username: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      email: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
       password: new FormControl(null, [Validators.required, Validators.minLength(3)])
     })
   }
