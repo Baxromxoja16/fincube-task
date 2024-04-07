6cacb7baa358555aacaf02dc808dd72c3c057b22
baxromxoja16 <aglamof@mail.ru>
2024-04-07 12:03:53 +0500

    feat :added login layout

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,81 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MaterialModule } from '../../shared/material.module';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, MaterialModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  hide = false
+
+  toggleBlock = true
+
+  logInForm:FormGroup;
+
+  registerForm!:FormGroup<any>;
+
+  documents:any[] = []
+
+  constructor() {
+    this.logInForm = new FormGroup({
+      username: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(3)])
+    })
+  }
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      firstName: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      lastName: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      birthday: new FormControl(null, [Validators.required]),
+      countryCode: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      phoneNumber: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      document: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+    }) as any
+
+  }
+
+  pickDate(picker:any) {
+    picker.open()
+  }
+
+  login() {
+    if(this.logInForm.valid) {
+    }
+  }
+
+  register(): void {
+    if(this.registerForm.valid) {
+      const {
+        email, password, firstName,
+        lastName, birthday,
+        countryCode, phoneNumber, document
+      } = this.registerForm.value;
+
+      const user:any = {
+        firstName, birthday, email,
+        lastName, password,
+        phoneNumber:document.code + phoneNumber,
+        citizenship: document.name,
+        login:email,
+        country: countryCode.name
+      }
+    }
+  }
+
+  loginWithGoogle() {
+
+  }
+
+  loginWithFacebook() {
+
+  }
 
 }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,12 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatTabsModule,
+    MatIconModule,
+    MatNativeDateModule,
+  ],
+  exports: [
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatTabsModule,
+    MatIconModule,
+    MatNativeDateModule,
+  ],
+  providers: [
+    MatDatepickerModule
   ]
 })
 export class MaterialModule { }
