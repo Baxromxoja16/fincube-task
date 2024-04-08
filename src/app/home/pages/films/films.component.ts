76336ba37b4abc533ae14421281045aa69e6ac5f
baxromxoja16 <aglamof@mail.ru>
2024-04-09 02:28:56 +0500

    feat: films template

--- src/app/home/pages/films/films.component.ts
+++ src/app/home/pages/films/films.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit, OnDestroy, WritableSignal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { MaterialModule } from '../../../shared/material.module';
+import { FilmsService, IFilms } from '../services/films.service';
 
 @Component({
   selector: 'app-films',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, DatePipe, RouterLink],
   templateUrl: './films.component.html',
   styleUrl: './films.component.scss'
 })
-export class FilmsComponent {
+export class FilmsComponent implements OnInit, OnDestroy {
+  films: WritableSignal<IFilms | any> = this.filmsService.filmsSig;
 
+  subscription = new Subscription();
+
+  constructor(private filmsService: FilmsService) {}
+
+  ngOnInit(): void {
+    const getFilms = this.filmsService.getFilms().subscribe();
+
+    console.log(this.filmsService.filmsSig());
+
+    this.subscription.add(getFilms);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
