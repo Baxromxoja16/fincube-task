e6867e0304f70587824ceb6909528a752d5aace2
baxromxoja16 <aglamof@mail.ru>
2024-04-09 13:33:19 +0500

    feat: added get people method

--- src/app/home/services/people.service.ts
+++ src/app/home/services/people.service.ts
@@ -1,9 +1,38 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { Subject, tap } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+
+export interface IPeoples {}
+export interface IPeople {}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
+  baseUrl = environment.apiUrl + 'people';
 
-  constructor() { }
+  peoplesSig = signal<IPeoples | any>({})
+
+  peopleSig = signal<IPeople | any>({})
+
+  people$ = new Subject<IPeople | any>();
+
+  constructor(private http: HttpClient) { }
+
+  getPeoples() {
+    return this.http.get(this.baseUrl).pipe(
+      tap((res) => {
+        this.peoplesSig.set(res);
+      })
+    );
+  }
+
+  getPeople(url: string) {
+    return this.http.get(url).pipe(
+      tap((res) => {
+        this.people$.next(res);
+      })
+    );
+  }
 }
