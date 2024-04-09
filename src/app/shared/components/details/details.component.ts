8b7c056aea2c5f0b40aff7377644fbbb3af90396
baxromxoja16 <aglamof@mail.ru>
2024-04-09 12:53:06 +0500

    feat: detail layout

--- src/app/shared/components/details/details.component.ts
+++ src/app/shared/components/details/details.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../material.module';
 import { DetailService } from '../../services/detail.service';
+import { SharedModule } from '../../shared.module';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, SharedModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss'
 })
