ab19ef187adb1ac5406e565bf5fc24e75fc92684
baxromxoja16 <aglamof@mail.ru>
2024-04-10 13:26:19 +0500

    feat: added title

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,15 +9,15 @@ export const routes: Routes = [
     redirectTo: '/auth',
     pathMatch: 'full',
   },
-  { path: 'auth', loadComponent: () => import('./auth/login.component').then(mod => mod.LoginComponent) },
+  { path: 'auth', loadComponent: () => import('./auth/login.component').then(mod => mod.LoginComponent), title: "Auth" },
   {
-    path: 'home',canActivateChild: [authGuardChild], canActivate:[authGuard], loadComponent: () => import('./home/home.component').then(mod => mod.HomeComponent), children: [
-      { path: 'people',  loadComponent: () => import('./home/pages/people/people.component').then(mod => mod.PeopleComponent), data: { animation: 'peoplePage' } },
-      { path: 'films',  loadComponent: () => import('./home/pages/films/films.component').then(mod => mod.FilmsComponent), data: { animation: 'filmsPage' } },
-      { path: 'planets', loadComponent: () => import('./home/pages/planets/planets.component').then(mod => mod.PlanetsComponent), data: { animation: 'planetsPage' } },
-      { path: 'ships', loadComponent: () => import('./home/pages/ships/ships.component').then(mod => mod.ShipsComponent), data: { animation: 'shipsPage' } },
-      { path: 'transport', loadComponent: () => import('./home/pages/tansport/tansport.component').then(mod => mod.TansportComponent), data: { animation: 'transportPage' } },
-      { path: 'detail', loadComponent: () => import('./shared/components/details/details.component').then(mod => mod.DetailsComponent), data: { animation: 'detailPage' } },
+    path: 'home',canActivateChild: [authGuardChild], canActivate:[authGuard], loadComponent: () => import('./home/home.component').then(mod => mod.HomeComponent), title: "Home", children: [
+      { path: 'people',  loadComponent: () => import('./home/pages/people/people.component').then(mod => mod.PeopleComponent), data: { animation: 'peoplePage' }, title: "People" },
+      { path: 'films',  loadComponent: () => import('./home/pages/films/films.component').then(mod => mod.FilmsComponent), data: { animation: 'filmsPage' }, title: "Films" },
+      { path: 'planets', loadComponent: () => import('./home/pages/planets/planets.component').then(mod => mod.PlanetsComponent), data: { animation: 'planetsPage' }, title: "Planets" },
+      { path: 'ships', loadComponent: () => import('./home/pages/ships/ships.component').then(mod => mod.ShipsComponent), data: { animation: 'shipsPage' }, title: "Ships" },
+      { path: 'transport', loadComponent: () => import('./home/pages/tansport/tansport.component').then(mod => mod.TansportComponent), data: { animation: 'transportPage' }, title: "Transport" },
+      { path: 'detail', loadComponent: () => import('./shared/components/details/details.component').then(mod => mod.DetailsComponent), data: { animation: 'detailPage' }, title: "Details" },
     ]
   },
   {
