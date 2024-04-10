d3a43c425316e13373984f8c66256846c90fbd4f
baxromxoja16 <aglamof@mail.ru>
2024-04-10 13:05:10 +0500

    feat: added planet service

---
+++ src/app/home/services/planets.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { Subject, tap } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+
+export interface IPlanets {}
+export interface IPlanet {}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlanetService {
+  baseUrl = environment.apiUrl + 'planet';
+
+  planetsSig = signal<IPlanets | any>({})
+
+  planetSig = signal<IPlanet | any>({})
+
+  planet$ = new Subject<IPlanet | any>();
+
+  constructor(private http: HttpClient) { }
+
+  getPlanets(page = '') {
+    return this.http.get(this.baseUrl + page).pipe(
+      tap((res) => {
+        this.planetsSig.set(res);
+      })
+    );
+  }
+
+  getPlanet(url: string) {
+    return this.http.get(url).pipe(
+      tap((res) => {
+        this.planet$.next(res);
+      })
+    );
+  }
+}
