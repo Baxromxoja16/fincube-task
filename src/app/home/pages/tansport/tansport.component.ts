24ba21cd4617f5b5e217ac6233fc799877fa46c7
baxromxoja16 <aglamof@mail.ru>
2024-04-10 15:27:49 +0500

    feat: added transport page

--- src/app/home/pages/tansport/tansport.component.ts
+++ src/app/home/pages/tansport/tansport.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
+import { MaterialModule } from '../../../shared/material.module';
+import { TransportService } from '../../services/transport.service';
 
 @Component({
   selector: 'app-tansport',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './tansport.component.html',
   styleUrl: './tansport.component.scss'
 })
 export class TansportComponent {
+  subscription = new Subscription();
+  transportSig = this.transportService.transportsSig;
+  pageSize = 10;
 
+  constructor(private transportService: TransportService) {}
+
+  ngOnInit(): void {
+    const getTransports = this.transportService.getAll().subscribe();
+
+    this.subscription.add(getTransports);
+  }
+
+  handlePageEvent(e: PageEvent) {
+    const page = '?page=' + (e.pageIndex + 1);
+    const getTransports = this.transportService.getAll(page).subscribe();
+    this.subscription.add(getTransports);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
