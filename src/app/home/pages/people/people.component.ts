9c55a42b483aa0e6dd55ebc6a274bcbed645f940
baxromxoja16 <aglamof@mail.ru>
2024-04-10 13:20:25 +0500

    feat: added subsciption for obs

--- src/app/home/pages/people/people.component.ts
+++ src/app/home/pages/people/people.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MaterialModule } from '../../../shared/material.module';
 import { PeopleService } from '../../services/people.service';
 import {PageEvent, MatPaginatorModule} from '@angular/material/paginator';
 import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-people',
@@ -11,18 +12,26 @@ import { CommonModule } from '@angular/common';
   templateUrl: './people.component.html',
   styleUrl: './people.component.scss'
 })
-export class PeopleComponent implements OnInit {
+export class PeopleComponent implements OnInit, OnDestroy {
   peopleSig = this.peopleService.peoplesSig
   pageSize = 10;
+  subscription = new Subscription()
 
   constructor(private peopleService: PeopleService) {}
 
   ngOnInit(): void {
-    this.peopleService.getPeoples().subscribe();
+    const getPeople = this.peopleService.getPeoples().subscribe();
+
+    this.subscription.add(getPeople);
   }
 
   handlePageEvent(e: PageEvent) {
     const page = '?page=' + (e.pageIndex + 1);
-    this.peopleService.getPeoples(page).subscribe();
+    const getPeople = this.peopleService.getPeoples(page).subscribe();
+    this.subscription.add(getPeople);
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
