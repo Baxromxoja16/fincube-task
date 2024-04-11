08abdf07f44f74c625e0436d28cf1aba6c3ea5c7
baxromxoja16 <aglamof@mail.ru>
2024-04-11 13:24:06 +0500

    feat: added ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,19 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { filmsReducer } from './state/reducers/films.reducer';
+import { FilmEffects } from './state/effects/films.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+    provideStore(),
+    provideEffects(FilmEffects),
+    provideState({ name: 'films', reducer: filmsReducer })]
 };
