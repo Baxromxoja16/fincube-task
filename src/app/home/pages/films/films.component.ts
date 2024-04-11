8adff67f73ced60154fe602bec6e328d0b84f857
baxromxoja16 <aglamof@mail.ru>
2024-04-11 13:26:41 +0500

    feat: added data to store

--- src/app/home/pages/films/films.component.ts
+++ src/app/home/pages/films/films.component.ts
@@ -1,9 +1,13 @@
 import { DatePipe } from '@angular/common';
-import { Component, OnInit, OnDestroy, WritableSignal, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, WritableSignal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { MaterialModule } from '../../../shared/material.module';
 import { DetailService } from '../../../shared/services/detail.service';
+import { loadFilms } from '../../../state/actions/films.actions';
+import { FilmState } from '../../../state/reducers/films.reducer';
+import { selectAllFilms } from '../../../state/selectors/films.selectors';
 import { CardComponent } from '../../components/card/card.component';
 import { FilmsService } from '../../services/films.service';
 import { IFilmListResponse } from './films.model';
@@ -15,25 +19,23 @@ import { IFilmListResponse } from './films.model';
   templateUrl: './films.component.html',
   styleUrl: './films.component.scss'
 })
-export class FilmsComponent implements OnInit, OnDestroy, AfterViewInit {
+export class FilmsComponent implements OnInit, OnDestroy {
   films: WritableSignal<IFilmListResponse> = this.filmsService.filmsSig;
+  films$ = this.store.select(selectAllFilms);
 
   subscription = new Subscription();
 
   constructor(
     private filmsService: FilmsService,
     private detailService: DetailService,
-    private router: Router
-    ) {}
+    private router: Router,
+    private store: Store<FilmState>
+    ) {
+    }
 
   ngOnInit(): void {
-    const getFilms = this.filmsService.getAll().subscribe();
+    this.store.dispatch(loadFilms());
 
-    this.subscription.add(getFilms);
-
-  }
-
-  ngAfterViewInit(): void {
   }
 
   detailCard(url: string) {
